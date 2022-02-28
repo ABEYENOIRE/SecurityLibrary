215bc45f0b4911a369d9c9ef9f9dd4b670894de1
ABEYE <70368444+ABEYENOIRE@users.noreply.github.com>
2022-02-28 11:46:18 +0100

    Update program
    by ABEYE

--- programme_application_projet_FINALE.ino
+++ programme_application_projet_FINALE.ino
@@ -6,6 +6,29 @@ String message;
 
 /* ========== */
 
+/* == Partie RFID (Roland) */
+
+// lien aide: https://www.electronique-mixte.fr/microcontrolleurs/rfid-controle-dacces-par-badge-avec-arduino/
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+    
+#define Max_Acces 3
+
+byte Count_acces=0; 
+byte CodeVerif=0; 
+byte Code_Acces[4]={0x12, 0x63, 0x6E, 0x1A};             //  ID DE LA CARTE (IL FAUT CHANGER LES NUMEROS APRES LES "x" CORRESPONDANT A LA CARTE/BADGE )
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+
+// Init array that will store new NUID 
+byte nuidPICC[4];
+
+/* ========== */
+
 /* == Partie Détecteur de passage (Oscar) */
 
 int entree = 4;
@@ -20,25 +43,37 @@ int nbrpers = 0;
 
 /* ========== */
 
+/* == Partie Lumière (Enzo) */
 
 int lum1 = 13;
 int lum2 = 8;
 int lum3 = 7;
 
+/* ========== */
+
 
 void setup() {
+
+  // ENZO 
   pinMode(lum1, OUTPUT);
   pinMode(lum2, OUTPUT);
   pinMode(lum3, OUTPUT);
 
+  // OSCAR
   pinMode(entree, INPUT);
   pinMode(sortie, INPUT);
-  
+
+  // ROLAND (APPLICATION)
   Serial.begin(9600);
   pinMode(9,OUTPUT); 
   digitalWrite(9,HIGH);
   Serial.println("Enter AT commands:");
   mySerial.begin(9600);
+
+  // ROLAND RFID
+  SPI.begin(); 
+  rfid.PCD_Init();
+  
 }
 
 void loop()
@@ -130,6 +165,78 @@ if(sortie != etatav2){
   
 }
   
+}
+
+/* ========== */
+
+/* == Partie RFID (Roland) == */
+
+void getBadge(){
+
+  // Initialisé la boucle si aucun badge n'est présent 
+  if ( !rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Vérifier la présence d'un nouveau badge 
+  if ( !rfid.PICC_ReadCardSerial())
+    return;
+
+  // Afffichage 
+  Serial.println(F("Un badge est détecté"));
+
+  // Enregistrer l’ID du badge (4 octets) 
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+
+  // Vérification du code 
+  CodeVerif = GetAccesState(Code_Acces,nuidPICC); 
+  if (CodeVerif != 1)
+  {
+    Count_acces+=1;
+    if(Count_acces==Max_Acces)
+    {
+     // Dépassement des tentatives (clignotement infinie) 
+     while(1)
+     { 
+      // Affichage 
+      Serial.println("Alarme!");
+     }
+    }
+    else
+    {
+      // Affichage 
+      Serial.println("Code érroné");
+    }
+  }
+  else
+  {
+    // Affichage 
+    Serial.println("Ouverture de la porte");
+    Count_acces=0; 
+  }
+
+  // Affichage de l'identifiant 
+  Serial.println(" L'UID du tag est:");
+  for (byte i = 0; i < 4; i++) 
+  {
+    Serial.print(nuidPICC[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  // Re-Init RFID
+  rfid.PICC_HaltA(); // Halt PICC
+  rfid.PCD_StopCrypto1(); // Stop encryption on PCD
+}
+
+byte GetAccesState(byte *CodeAcces,byte *NewCode) {
+  byte StateAcces=0; 
+  if ((CodeAcces[0]==NewCode[0])&&(CodeAcces[1]==NewCode[1])&&
+  (CodeAcces[2]==NewCode[2])&& (CodeAcces[3]==NewCode[3]))
+    return StateAcces=1; 
+  else
+    return StateAcces=0; 
 }
 
 /* ========== */
